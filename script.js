c38e26b3d29b63500ee7521b4221d1bff8980a25
lesminots <lesminots.off@gmail.com>
2020-05-28 16:07:28 +0200

    28052020

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function click(){
+    si a click alors affiche a + func slider() 
+    si b click alors affiche b + func slider()
+}
+
+
+
+
+
+
+
+
+
+function imgoff() {
+    document.getElementById("").style.display = "none";
+    document.getElementById("").style.display = "none";
+
+};
+
+function onimg() {
+    document.getElementById("slider").style.display = "block";
+    document.getElementById("slider_back").style.display = "block";
+};
